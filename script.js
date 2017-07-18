7a75fc441dd8e463a2b932e9a0b8fc1069f33a95
Jason Lucas <jlucas5280@gmail.com>
2017-07-18 07:32:39 -0600

    ui

---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+// video volume control
+var vid = document.getElementById("barry");
+vid.currentTime = 38;
+vid.volume = .6;
---
+++ scripts/Trie.js
@@ -0,0 +1,28 @@
+import Node from './Node'
+
+export default class Trie {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(data) {
+    if (this.root === null) {
+      this.root = new Node();
+    }
+
+    
+
+  }
+
+  count() {
+
+  }
+
+  suggest() {
+
+  }
+
+  select() {
+
+  }
+}
